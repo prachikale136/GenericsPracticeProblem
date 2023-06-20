7bafb243ca4a308a0fd92a325ebd6a07bee313ba
Prachi <prachikale136@gmail.com>
2023-06-20 14:42:35 +0530

    [Prachi] Add: delete elemnt from array

---
+++ GenericsProblem/GenericsProblem/DeleteArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class DeleteArray
+    {
+        public static void DeleteElement(int []arr ,int n, int x)  // here x is element in array
+        {
+            int i;
+            for (i = 0;i<n;i++) 
+            {
+                if (arr[i] == x)
+                    break;
+            }
+            //if x found in array then 
+            if(i < n)   
+            {
+                //redude size of array and move element one space ahead
+                n = n - 1;
+                for(int j = i;j<n;j++) 
+                {
+                    arr[j] = arr[j + 1];
+                }
+                //return n;
+            }
+            
+        }
+    }
+}
---
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+          //  DeleteArray deleteArray = new DeleteArray();
+            int[] arr = { 10, 20, 30, 5, 45, 6, 250 };
+            int n = arr.Length;
+            DeleteArray.DeleteElement(arr, n, 45);
+            Console.WriteLine("Array after delete elements");
+            for(int i =0;i<n-1;i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+         
+    }
+}
